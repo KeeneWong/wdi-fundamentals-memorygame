cafd24f49b4807e437bb4ef5dc31cb80ea90ca67
Keene <keenewongofung@gmail.com>
2019-06-16 16:30:31 +0800

    create game board

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -30,20 +30,37 @@ cardImage: "images/king-of-diamonds.png",
 var cardsInPlay = [];
 
 
+function createBoard(){
+ 	
+ 	for(var i = 0; i < cards.length; i += 1){
+
+ 		var cardElement = document.createElement("img");
+ 		cardElement.setAttribute("src", "images/back.png");
+ 		cardElement.setAttribute("data-id",i);
+ 		cardElement.addEventListener("click", flipCard);
+ 		document.getElementById("game-board").appendChild(cardElement);
+
+ 	} 
+
+
+
+
+}
 
 
 function checkForMatch(){
 
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+  alert("You found a match!");
 } else {
-  console.log("Sorry, try again.");
+  alert("Sorry, try again.");
 }
 }
 
 
-function flipCard(cardId){
+function flipCard(){
 
+	var cardId = this.getAttribute('data-id');
 
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
@@ -51,11 +68,17 @@ function flipCard(cardId){
 
 	cardsInPlay.push(cards[cardId].rank);
 
+	this.setAttribute("src", cards[cardId].cardImage);
+
 	while(cardsInPlay.length===2){
 	checkForMatch();
 	break;
 }
+	
 }
+
+createBoard();
+
 
 
 
